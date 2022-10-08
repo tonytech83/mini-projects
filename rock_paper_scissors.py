355365d8e6d98ae3d4ac3f302238e75db04d333a
Anton Petrov <a.petrov.83@gmail.com>
2022-10-08 20:59:08 +0300

    add print of player and computer chose changed the output of game result changed output of final result

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,6 +1,6 @@
 import random
 
-computer_options = ['r', 'p', 's']
+computer_options = ['rock', 'paper', 'scissors']
 
 player_move = input('Choose [r]ock, [p]aper, [s]cissors or [e]nd: ')
 your_wins = 0
@@ -8,19 +8,27 @@ your_loses = 0
 draws = 0
 
 while player_move != 'e':
+    if player_move == 'r':
+        player_move = 'rock'
+    elif player_move == 'p':
+        player_move = 'paper'
+    elif player_move == 's':
+        player_move = 'scissors'
     computer_move = random.choice(computer_options)
+    print(f'Your chose: {player_move}')
+    print(f'Computer chose: {computer_move}')
     if player_move == computer_move:
         draws += 1
-        print('Draw!')
-    elif player_move == 'r' and computer_move == 's' \
-            or player_move == 'p' and computer_move == 'r' \
-            or player_move == 's' and computer_move == 'p':
+        print('>>> Draw! <<<')
+    elif player_move == 'rock' and computer_move == 'scissors' \
+            or player_move == 'paper' and computer_move == 'rock' \
+            or player_move == 'scissors' and computer_move == 'paper':
         your_wins += 1
-        print('You win!')
+        print('>>> You win! <<<')
     else:
         your_loses += 1
-        print('You lose!')
+        print('>>> You lose! <<<')
 
     player_move = input('Choose [r]ock, [p]aper, [s]cissors or [e]nd: ')
 
-print(f'---------------------\nThe final result is:\nWins: {your_wins}\nLoses: {your_loses}\nDraws: {draws}')
+print(f'------------------------\n| The final result is:\n| Wins: {your_wins}\n| Loses: {your_loses}\n| Draws: {draws}\n------------------------')
