b5b7986263861ab9faf2281c56b71860da109e98
Anton Petrov <a.petrov.83@gmail.com>
2022-11-28 20:33:22 +0200

    Created menubar

--- Excel_to_CSV/excel_to_csv.py
+++ Excel_to_CSV/excel_to_csv.py
@@ -4,6 +4,11 @@ from pathlib import Path
 
 
 def is_valid_path(file_path):
+    """
+    This function checks if the paths exist.
+    :param file_path: str
+    :return: boolean
+    """
     if file_path and Path(file_path).exists():
         return True
     sg.popup_error('Filepath not correct!')
@@ -11,12 +16,25 @@ def is_valid_path(file_path):
 
 
 def display(excel_file_path, sheet_name):
+    """
+    This function shown quick preview of Excel file
+    :param excel_file_path: str
+    :param sheet_name: str
+    """
     df = pd.read_excel(excel_file_path, sheet_name)
     file_name = Path(excel_file_path).name
     sg.popup_scrolled(df.dtypes, '=' * 50, df, title=file_name)
 
 
 def convert(excel_file_path, output_folder, sheet_name, separator, decimal):
+    """
+    This function converts Excel file to csv file and saved new file in Output folder.
+    :param excel_file_path: str
+    :param output_folder: str
+    :param sheet_name: str
+    :param separator: str
+    :param decimal: str
+    """
     df = pd.read_excel(excel_file_path, sheet_name)
     file_name = Path(excel_file_path).stem
     output_file = Path(output_folder) / f'{file_name}.csv'
@@ -25,6 +43,12 @@ def convert(excel_file_path, output_folder, sheet_name, separator, decimal):
 
 
 def settings_window(settings):
+    """
+    Creates new window with settings from where can be changed separator, decimal and sheet name.
+    After click 'Save settings' button all setting are saved in config.ini file.
+    :param settings: str
+    """
+    # Settings window definition
     layout = [[sg.Text('Settings')],
               [sg.Text('Separator'), sg.Input(settings['CSV']['separator'], s=1, key='-SEP-'),
                sg.Text('Decimal'), sg.Combo(settings['CSV']['decimal'].split('|'),
@@ -33,7 +57,7 @@ def settings_window(settings):
                sg.Text('Sheet name:'), sg.Input(settings['EXCEL']['sheet_name'], s=20, key='-SHEET_NAME-')],
               [sg.Button('Save settings', s=20)]]
 
-    window = sg.Window('Settings', layout, modal=True)
+    window = sg.Window('Settings', layout, modal=True, use_custom_titlebar=True)
     while True:
         event, values = window.read()
         if event == sg.WINDOW_CLOSED:
@@ -49,18 +73,30 @@ def settings_window(settings):
 
 
 def main():
-    # GUI definition
+    """
+    This function creates main application window and handle actions for all buttons.
+    """
+
+    # Menu definition
+    menu = [['Help', ['Settings', 'About']]]
+
+    # Main GUI definition
     layout = [
-        [sg.Text('Input file:'), sg.Input(key="-IN-"), sg.FileBrowse(file_types=(('Excel Files', '*.xls*'),))],
-        [sg.Text('Output folder:'), sg.Input(key="-OUT-"), sg.FolderBrowse()],
-        [sg.Exit(), sg.Button('Settings'), sg.Button('Display Excel file'), sg.Button('Convert to CSV')],
+        [sg.MenubarCustom(menu, tearoff=False)],
+        [sg.Text('Input file:', s=11, justification='r'), sg.Input(key="-IN-"),
+         sg.FileBrowse(file_types=(('Excel Files', '*.xls*'),))],
+        [sg.Text('Output folder:', s=11, justification='r'), sg.Input(key="-OUT-"), sg.FolderBrowse()],
+        [sg.Button('Convert to CSV', s=15), sg.Button('Display Excel file', s=15),
+         sg.Button('Settings', s=15), sg.Exit(s=15, button_color='tomato')],
     ]
 
     window_title = settings['GUI']['title']
-    window = sg.Window(window_title, layout)
+    window = sg.Window(window_title, layout, use_custom_titlebar=True)
 
     while True:
+
         event, values = window.read()
+
         if event in (sg.WINDOW_CLOSED, 'Exit'):
             break
 
@@ -80,6 +116,9 @@ def main():
                     separator=settings['CSV']['separator'],
                     decimal=settings['CSV']['decimal'],
                 )
+
+        if event == 'About':
+            sg.popup(window_title, 'version 0.1', 'Convert Excel file to CSV')
 
     window.close()
 
