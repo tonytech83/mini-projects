3fc7099de0117d2ede83b51fea4d8a557e93023e
tonytech83 <a.petrov.83@gmail.com>
2026-01-20 13:14:16 +0200

    fix some issues

--- Python/random_sentences_generator.py
+++ Python/random_sentences_generator.py
@@ -5,7 +5,7 @@ def get_random(lst: list[str]):
     return random.choice(lst)
 
 
-lst: list[list] = [
+lst: list[list[str]] = [
     ["Anton", "Petar", "Dian", "Teodor", "Junian", "Kiril"],
     ["Sofia", "Stara Zagora", "Plovdiv", "Galabovo", "Varna"],
     ["coding on", "eats", "holds", "plays with", "brings"],
@@ -14,12 +14,13 @@ lst: list[list] = [
     ["near the sea", "at home", "at work", "in the park"],
 ]
 
-input("Hello! This will be your first random sentence:")
+input("Hello! This will be your first random sentence.")
 
-while True:
+usr_input: str = ""
+while usr_input != "q":
     result = [get_random(x) for x in lst]
 
     print(
         f"{result[0]} from {result[1]} {result[2]} {result[3]} {result[4]} {result[5]}",
     )
-    input("Hit [Enter] to generate a new one!")
+    usr_input = input("Hit [Enter] to generate a new one or [q] to quit!")
